b024ba62de44c7e05c1388129aae88a81640e214
Angel G. Cuartero <anhelido@gmail.com>
2019-03-09 19:29:15 +0100

    User keys added in title screen.

--- pong.c
+++ pong.c
@@ -161,8 +161,10 @@ int main(void)
                 DrawText("PONG", 120, 20, 120, GRAY);
                 DrawText("Based on Atari PONG", 120, 140, 60, GRAY);
                 DrawText("Programmed by Angel G. Cuartero", 120, 220, 20, GRAY);
-                DrawText("Press ENTER to PLAY", 120, 420, 20, GRAY);
-                DrawText("Press ESCAPE to QUIT", 120, 450, 20, GRAY);
+                DrawText("Player 1: Q, A", 120, 390, 20, GRAY);
+                DrawText("Player 2: I, J", 120, 420, 20, GRAY);
+                DrawText("Press ENTER to PLAY", 120, 450, 20, GRAY);
+                DrawText("Press ESCAPE to QUIT", 120, 480, 20, GRAY);
 
             } break;
             case GAMEPLAY:
